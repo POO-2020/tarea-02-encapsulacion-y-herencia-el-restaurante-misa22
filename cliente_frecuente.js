544fd57193b0786405c6166740003e5fac7e560f
misa22 <misael.solano.m@gmail.com>
2020-03-24 19:01:25 -0600

    cambio en clases

--- cliente_frecuente.js
+++ cliente_frecuente.js
@@ -1,4 +1,4 @@
-import Cliente from "./Cliente.js";
+import Cliente from "./cliente.js";
 
 export default class ClienteFrecuente extends Cliente {
     constructor({ nombre, direccion, telefono, fechaRegistro }) {
--- pedido.js
+++ pedido.js
@@ -11,18 +11,23 @@ export default class Pedido {
      * @param {Tiempo} hora 
      * @param {Cliente} cliente 
      */
-    constructor(fecha, hora, cliente) {
+    constructor({ fecha, hora, cliente }) {
         this._fecha = fecha;
         this._hora = hora;
         this._cliente = cliente;
         this._elementosPedidos = [];
+        this._numeroPedido = Date.now();
     }
+
+    esIgual = pedido =>
+        this.getResumen() == pedido.getResumen();
+
     getNumeroElementos = _ => this._elementosPedidos.length;
 
-    getProductos = _ => {
+    getNumeroProductos = _ => {
         let nProductos = 0;
         this._elementosPedidos.forEach(elemento => {
-            nProductos+=elemento.cantidad;
+            nProductos += elemento.getCantidad();
         });
         return nProductos;
     }
@@ -30,17 +35,27 @@ export default class Pedido {
     getCostoTotal = _ => {
         let costo = 0;
         this._elementosPedidos.forEach(elemento => {
-            costo += elemento._cantidad * elemento._producto._precio._valor;
+            costo += elemento.getCosto();
         });
-        return new Precio(costo).getPrecio();
+        return new Precio(Number(costo)).getPrecio();
     }
 
     getResumen = _ =>
-        `${this._fecha.getFecha()}  ${this._hora.getFormato12()}\n-${this.getNumeroElementos()} elementos con ${this.getProductos()} productos-\ntotal: ${this.getCostoTotal()}`;
+        `${
+        this._fecha.getFecha()
+        }  ${
+        this._hora.getFormato12()
+        }\n-${
+        this.getNumeroElementos()
+        } elementos con ${
+        this.getNumeroProductos()
+        } productos-\ntotal: ${
+        this.getCostoTotal()
+        }`;
     /**
      * @param {ElementoPedido} elementos
      */
     agregarElemento = elemento => this._elementosPedidos.push(elemento);
 
-    listarElementos = _ => this._elementosPedidos.forEach(elemento => console.log(elemento.getDescripcion(), '\n'));
+    listarElementos = _ => this._elementosPedidos.forEach(elemento => console.log(elemento.getDescripcion()));
 }
